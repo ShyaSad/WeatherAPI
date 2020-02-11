815f141699632c9611807f4d40b8285d92617ed5
shyasadeghi@gmail.com <shyasadeghi@gmail.com>
2020-02-11 14:30:13 -0800

    fixed https

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $("#search-button").on("click", function () {
     let userInput = $(".form-control").val()
     console.log(userInput);
-    let queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + userInput + "&units=imperial&appid=04d1faa1ca8bf65e3e55b742a828000b"
+    let queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + userInput + "&units=imperial&appid=04d1faa1ca8bf65e3e55b742a828000b"
     // let jumboExist = true;
 
     $.ajax({
